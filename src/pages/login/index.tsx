d0f6fe8af09d419678204d9e00c728daf9aec698
DEV1GOR <igormatheus.0702@gmail.com>
2025-03-12 02:56:10 -0300

    Primeiros passos para a tela de login

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import {
+Text, View,
+} from 'react-native';
+
+import { style } from "./style";
+
+export default function login(){
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+            </View>
+          
+            <View style={style.boxMid}>
+
+            </View>
+           
+            <View style={style.boxBottom}>
+
+            </View>
+        </View>
+    )
+}
